67292fb652e88fcc6251529c662e2176cfb7f19c
Ifeanyi <doc3coder@gmail.com>
2020-07-16 20:33:11 +0100

    connected cleardb

---
+++ withme.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 16, 2020 at 08:18 PM
+-- Server version: 5.7.30-0ubuntu0.18.04.1
+-- PHP Version: 7.1.33-16+ubuntu18.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `withme`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `preregistered`
+--
+
+CREATE TABLE `preregistered` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `preregistered`
+--
+
+INSERT INTO `preregistered` (`id`, `email`) VALUES
+(1, 'yemi@web.com'),
+(2, 'yemi@web.com'),
+(3, 'here@web.com'),
+(4, 'test@web.com'),
+(5, 'yemi@web.com'),
+(6, 'test@web.com'),
+(7, 'yemi@web.com'),
+(8, 'yemi@web.com'),
+(9, 'yemi@web.com'),
+(10, 'yemi@web.com'),
+(11, 'yemi@web.com'),
+(12, 'yemi@web.com'),
+(13, 'yemi@web.com'),
+(14, 'yemi@web.com'),
+(15, 'yemi@web.com'),
+(16, 'premgitalng@gmail.com'),
+(17, 'premgitalng@gmail.com'),
+(18, 'premgitalng@gmail.com'),
+(19, 'premgitalng@gmail.com'),
+(20, 'premgitalng@gmail.com'),
+(21, 'premgitalng@gmail.com'),
+(22, 'premgitalng@gmail.com'),
+(23, 'premgitalng@gmail.com'),
+(24, 'premgitalng@gmail.com'),
+(25, 'premgitalng@gmail.com'),
+(26, 'premgitalng@gmail.com'),
+(27, 'premgitalng@gmail.com'),
+(28, 'premgitalng@gmail.com'),
+(29, 'premgitalng@gmail.com'),
+(30, 'premgitalng@gmail.com'),
+(31, 'premgitalng@gmail.com'),
+(32, 'premgitalng@gmail.com'),
+(33, 'premgitalng@gmail.com'),
+(34, 'premgitalng@gmail.com'),
+(35, 'premgitalng@gmail.com'),
+(36, 'premgitalng@gmail.com'),
+(37, 'premgitalng@gmail.com'),
+(38, 'premgitalng@gmail.com'),
+(39, 'premgitalng@gmail.com'),
+(40, 'premgitalng@gmail.com'),
+(41, 'premgitalng@gmail.com'),
+(42, 'premgitalng@gmail.com'),
+(43, 'premgitalng@gmail.com'),
+(44, 'premgitalng@gmail.com'),
+(45, 'premgitalng@gmail.comefcrdc'),
+(46, 'here@web.com'),
+(47, 'here@web.com'),
+(48, 'premgitalng@gmail.com'),
+(49, 'premgitalng@gmail.com'),
+(50, 'premgitalng@gmail.com'),
+(51, 'premgitalng@gmail.com'),
+(52, 'premgitalng@gmail.com'),
+(53, 'premgitalng@gmail.com'),
+(54, 'yemi@web.com'),
+(55, 'premgitalng@gmail.com'),
+(56, 'premgitalng@gmail.com'),
+(57, 'premgitalng@gmail.com'),
+(58, 'premgitalng@gmail.com'),
+(59, 'yemi@web.com'),
+(60, 'yemi@web.com'),
+(61, 'premgitalng@gmail.com'),
+(62, 'premgitalng@gmail.com'),
+(63, 'premgitalng@gmail.com'),
+(64, 'premgitalng@gmail.com'),
+(65, 'yemi@web.com'),
+(66, 'abioroimodoye@gmail.com'),
+(67, 'abioroimodoye@gmail.com'),
+(68, 'abioroimodoye@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `preregistered`
+--
+ALTER TABLE `preregistered`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `preregistered`
+--
+ALTER TABLE `preregistered`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
